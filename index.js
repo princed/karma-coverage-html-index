6629b48511462d69f5a983f0c923db67d29d9a54
Eugene Datsky <eugene@datsky.ru>
2016-02-25 15:42:08 +0300

    :lipstick:

--- index.js
+++ index.js
@@ -5,11 +5,14 @@ var util = require('util');
 var linkTemplate = '<li><a class="link%s" href="%s">%s</a></li>';
 var linkActiveClass = ' link_active';
 
+var templateFile = 'index.html';
+var indexFile = 'index.html';
+
 function reporter(rootConfig, helper, logger) {
   var log = logger.create('coverage-html-index');
 
   var config = rootConfig.coverageReporter || {};
-  var reporterConfig;
+  var reporterConfig = {};
 
   if (config.reporters) {
     config.reporters.forEach(function (currentReporterConfig) {
@@ -20,22 +23,32 @@ function reporter(rootConfig, helper, logger) {
   }
 
   // Taken from karma-coverage
-  var mainDir = reporterConfig && reporterConfig.dir || config.dir;
+  var mainDir = reporterConfig.dir || config.dir;
   var resolvedOutputDir = path.resolve(rootConfig.basePath, mainDir);
   var outputDir = helper.normalizeWinPath(resolvedOutputDir);
 
-  var coverageIndexPath = path.resolve(outputDir, 'index.html');
-  var templatePath = path.resolve(__dirname, 'index.html');
+  var coverageIndexPath = path.resolve(outputDir, indexFile);
+  var templatePath = path.resolve(__dirname, templateFile);
 
   this.onRunComplete = function (browsers) {
-    var browsersArray = browsers.serialize().sort(function (a, b) {
-      return a.name < b.name;
-    });
+    var browsersArray = browsers.
+      serialize().
+      sort(function (a, b) {
+        return a.name > b.name;
+      }).
+      map(function (browser) {
+        var browserDir = typeof reporterConfig.subdir === 'function' ? reporterConfig.subdir(browser.name) : browser.name;
+
+        return {
+          name: browser.name.replace(' 0.0.0', ''), // strip meaningless versions
+          path: path.join(encodeURIComponent(browserDir), indexFile)
+        }
+      });
+
     var browsersList = browsersArray.map(function (browser, index) {
-      var shortBrowserName = browser.name.replace(' 0.0.0', ''); // strip meaningless versions
       var additionalClass = index === 0 ? linkActiveClass : '';
 
-      return util.format(linkTemplate, additionalClass, browser.name, shortBrowserName);
+      return util.format(linkTemplate, additionalClass, browser.path, browser.name);
     }).join('\n');
 
     function writeFile(err, template) {
@@ -44,7 +57,8 @@ function reporter(rootConfig, helper, logger) {
         return;
       }
 
-      var content = util.format(template.toString(), browsersList, browsersArray[0].name);
+      var defaultPath = browsersArray[0].path;
+      var content = util.format(template.toString(), browsersList, defaultPath);
 
       log.debug('Writing coverage index.html to %s', coverageIndexPath);
       fs.writeFile(coverageIndexPath, content, function (err) {
